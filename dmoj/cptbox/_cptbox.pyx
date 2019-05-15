098ec31eac6ff2423b17c2f99fe96b456f052a61
Tudor Brindus <me@tbrindus.ca>
2019-05-15 21:22:56 +0300

    cptbox: Set RLIMIT_FSIZE on submission processes
    By itself this doesn't do much, but it's just another layer of defence
    we ought to have. Note that a misconfigured whitelist allowing file
    writes can still fill up the disk, albeit slower and in increments of
    the disk block size.

--- dmoj/cptbox/_cptbox.pyx
+++ dmoj/cptbox/_cptbox.pyx
@@ -81,6 +81,7 @@ cdef extern from 'helper.h' nogil:
         unsigned int cpu_time # ask linus how this counts the CPU time because it SIGKILLs way before the real time limit
         unsigned long personality # so we can do things like disable ASLR without toasting security on entire system
         int nproc
+        int fsize
         char *file
         char *dir
         char **argv
@@ -400,6 +401,7 @@ cdef class Process:
         config.memory = self._child_memory
         config.cpu_time = self._cpu_time
         config.nproc = self._nproc
+        config.fsize = self._fsize
         config.personality = self._child_personality
         config.file = file
         config.dir = chdir
